e7f2090c0ae6fc50536c5209044eb7e9e07eab1b
DLegorreta <d.legorreta.anguiano@gmail.com>
2019-04-28 09:40:53 -0500

    test file string

---
+++ string.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int my_strlen(char *s)
+{
+   char *p = s;
+
+   while (*p)
+      ++p;
+
+   return (p - s);
+}
+
+int main(void)
+{
+   int i;
+   char *s[] = 
+   {
+      "Git tutorials",
+      "Tutorials Point"
+   };
+
+   for (i = 0; i < 2; ++i)
+      
+   printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
+
+
+
+
+
+
+
+
